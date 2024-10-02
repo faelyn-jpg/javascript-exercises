456fecf0cd052f89b1044027ea31a46b98cf7d86
faelyn-jpg <169906179+faelyn-jpg@users.noreply.github.com>
2024-10-02 14:34:55 +1300

    add solution to palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+  let alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789'
+  let cleanSentence = sentence
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('')
 
-};
+  let reverseSentence = cleanSentence.split('').reverse().join('')
+  return cleanSentence === reverseSentence
+}
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes
